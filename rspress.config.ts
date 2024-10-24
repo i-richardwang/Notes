eefb676f0f400b8796804c6ed1ff6db7f87471d8
Richard Wang <git@xmail.ing>
2024-10-24 12:02:34 +0800

    Add rspress-plugin-sitemap dependency and configure it in rspress.config.ts.

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 import fileTree from 'rspress-plugin-file-tree';
+import sitemap from "rspress-plugin-sitemap";
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -8,8 +9,8 @@ export default defineConfig({
   description: '关于AI、数据分析、及实用工具的实践和经验分享',
   icon: '/rspress-icon.png',
   // logo: {
-    // light: '/rspress-light-logo.png',
-    // dark: '/rspress-dark-logo.png',
+  // light: '/rspress-light-logo.png',
+  // dark: '/rspress-dark-logo.png',
   // },
   globalStyles: path.join(__dirname, 'styles', 'index.css'),
   themeConfig: {
@@ -17,6 +18,11 @@ export default defineConfig({
       { icon: 'github', mode: 'link', content: 'https://github.com/i-Richard-me' },
     ],
   },
-  plugins: [fileTree()],
+  plugins: [
+    fileTree(),
+    sitemap({
+      domain: "https://blog.richardwang.me",
+      defaultChangeFreq: "weekly",
+    }),
+  ],
 });
-  
